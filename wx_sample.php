e5fa555751d64460fa229bb4b02dff483e5f7c1f
mz121star <mz121star@gmail.com>
2014-06-27 13:53:40 +0800

    aa

--- wx_sample.php
+++ wx_sample.php
@@ -20,12 +20,34 @@ class wechatCallbackapiTest
         	exit;
         }
     }
+         /**
+          * 发送post请求
+          * @param string $url 请求地址
+          * @param array $post_data post键值对数据
+          * @return string
+          */
+        public function send_post($url, $post_data) {
 
+           $postdata = http_build_query($post_data);
+           $options = array(
+             'http' => array(
+               'method' => 'POST',
+               'header' => 'Content-type:application/x-www-form-urlencoded',
+               'content' => $postdata,
+               'timeout' => 15 * 60 // 超时时间（单位:s）
+             )
+           );
+           $context = stream_context_create($options);
+           $result = file_get_contents($url, false, $context);
+
+           return $result;
+         }
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
+        echo   $this->send_post('http://print.wx.dlwebs.com/wx.php',$postStr);
+        exit;
       	//extract post data
 		if (!empty($postStr)){
                 
