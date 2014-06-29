aa8d19e4f87d6f4abc8b1a6ec2184a34d1bdcd56
mz121star <mz121star@gmail.com>
2014-06-29 23:01:54 +0800

    zoom picture

--- zoom.php
+++ zoom.php
@@ -6,6 +6,7 @@
                             $fromuser= $_GET["id"];
                             echo "执行getPicByUID:".date("Y-m-d H:i:s",time());
                              $picurl=$wcHelper->getPicByUID($fromuser);
+        $picurl=$_GET["pic"];
                                  echo "执行完getPicByUID:".date("Y-m-d H:i:s",time());
 
 //获取图片原始宽高，计算缩小比例
