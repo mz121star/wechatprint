c042ef0544978c9f74191151ef5475d671911283
mz121star <mz121star@gmail.com>
2014-06-29 11:55:02 +0800

    zoom picture

--- zoom.php
+++ zoom.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>裁剪图片</title>
 
 <style type="text/css">
