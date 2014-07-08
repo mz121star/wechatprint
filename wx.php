4b9a603efff762b8c9ccd25d0e50af4d2f59e518
mz121star <mz121star@gmail.com>
2014-07-09 00:20:41 +0800

    zoom picture

--- wx.php
+++ wx.php
@@ -176,15 +176,19 @@ class wechatCallbackapiTest
 		$cuser=$object->FromUserName;
         $keyword = trim($object->Content);
 
-      //$content= file_get_contents("http://www.xiaohuangji.com/web.php?callback=jQuery17101399328545667231_1404833649724&para=".$keyword."&_=".time());
-       // $content= str_replace("test(\"", "", $content);
-       // $content  = str_replace("\")", "", $content);
-      //  $content= $this->unicode_decode($content) ;
+       $content= file_get_contents("http://www.xiaohuangji.com/web.php?callback=jQuery17101399328545667231_1404833649724&para=".$keyword."&_=".time());
+ $this->logger($content);
+         $content= str_replace("test(\"", "", $content);
+         $content  = str_replace("\")", "", $content);
+        $content= $this->unicode_decode($content) ;
+    $this->logger($content);
          $result = $this->transmitText($object, $keyword);
 
+ $this->logger($result);
 
 
         return $result;
+
     }
 
     //接收图片消息
