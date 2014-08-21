94c94a54cef1556d188959d7dc399d32e025d058
mz121star <mz121star@gmail.com>
2014-08-21 11:15:33 +0800

    ss

--- crop.php
+++ crop.php
@@ -43,7 +43,7 @@ $height = intval($cropheight*$sxbl);
 
   $finalimage= imageHelper::imagecropper($filepath,$src,$imgArray,800,600);
    $wcHelper=new wechatHelper();
-   $wcHelper->inserPic($_GET["id"],$picurl);
+   $wcHelper->inserPic($_GET["id"],"/uploads/"+$finalimage);
   echo $finalimage;
 
 
