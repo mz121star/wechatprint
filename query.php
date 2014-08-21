2c302ba5cd95cc7deadd01f1933143cb62e63735
mz121star <mz121star@gmail.com>
2014-08-21 13:04:55 +0800

    a

--- query.php
+++ query.php
@@ -1,15 +1,15 @@
 <?php
       require('wechatHelper.php');
-
+        $wcHelper=new wechatHelper();
       if(isset($_GET["uid"]) && $_GET["uid"]){
 
           $uid=$_GET["uid"];
-          $wcHelper-> setNoPrintPics($uid);
+          $wcHelper->setNoPrintPics($uid);
           echo "printed";
        }
        else{
 
-                 $wcHelper=new wechatHelper();
+
                  echo json_encode( $wcHelper->getNoPrintPics());
        }
 
